724464469c0cfeb954c7e4969d763baab809c74f
aassaintpierre <mathurinsarah42@gmail.com>
2022-08-14 16:17:53 -0400

    Second activity design

--- app/src/main/java/com/codepath/apps/restclienttemplate/DetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailActivity.java
@@ -17,24 +17,38 @@ public class DetailActivity extends AppCompatActivity {
     private   TextView tvView;
     private TextView txtView;
     private ImageView imaj;
+    ImageView star;
+    ImageView retweet;
+    ImageView share;
+    ImageView comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        tvView = findViewById(R.id.tvname);
-        txtView = findViewById(R.id.tvbody2);
+        txtView = findViewById(R.id.tvname);
+        tvView = findViewById(R.id.tvbody2);
         imaj = findViewById(R.id.imaj);
+        star = findViewById(R.id.star);
+        retweet = findViewById(R.id.retweet);
+        comment = findViewById(R.id.comment);
+        share = findViewById(R.id.share);
 
         Tweet tweet = Parcels.unwrap(getIntent().getParcelableExtra("Tweet"));
 
         tvView.setText(tweet.getBody());
-        txtView.setText(tweet.getUser().getName());
+        txtView.setText(tweet.user.name);
 
         Glide.with(this)
                 .load(tweet.user.profileImageUrl)
                 .transform(new RoundedCorners(50))
                 .into(imaj);
+
+
+        star.setImageResource(R.drawable.ic_outline_star_border_24);
+        share.setImageResource(R.drawable.ic_outline_share_24);
+        comment.setImageResource(R.drawable.ic_outline_mode_comment_24);
+        retweet.setImageResource(R.drawable.ic_repeat);
     }
 }
